53a892c217472a4dcc3985ab4d409cbd91831ae3
Osakpolor Obaseki <obasekiosa@gmail.com>
2021-12-21 17:26:01 +0100

    Scafold PreviewCard component

--- src/components/preview_card/PreviewCard.js
+++ src/components/preview_card/PreviewCard.js
@@ -1,5 +1,23 @@
+import "./PreviewCard.scss";
+import logo from "../../assets/images/icon-luxury.svg";
+
+
 const PreviewCard = () => {
-    return null;
+
+    const brightYellow = "hsl(31, 77%, 52%)";
+    const style = {
+        backgroundColor: brightYellow,
+    };
+
+    return (
+        <div className="PreviewCard" style={style}>
+            <img src={logo} alt="img"/> 
+            <h2>Sedans</h2>
+            <p>Choose a sedan for its affordability and excellent fuel economy. Ideal for cruising in the city
+                or on your next road trip.</p>
+            <button>Learn more</button>
+        </div>
+    );
 }
 
 
